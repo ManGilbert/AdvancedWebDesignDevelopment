99ef55327d606e1b423b4dd50727e3483d8366be
Gilbert <gimanishimwe123@gmail.com>
2026-03-02 19:27:43 +0200

    php lesson

---
+++ php/hello.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<?php
+$answer = 42;
+echo "The answer to the utltimate question of lofe $answer" ;
+?>
+
+<html>
+
+<head>
+    <title>Hello PHP</title>
+</head>
+
+<body>
+    <h1>Hello from Dr.Chunks!</h1>
+</body>
+
+</html>
