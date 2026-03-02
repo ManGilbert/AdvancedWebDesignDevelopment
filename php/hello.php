e4fc19ad079532d9d99197f972b93769795a6718
Gilbert <gimanishimwe123@gmail.com>
2026-03-02 20:23:08 +0200

    php lesson

--- php/hello.php
+++ php/hello.php
@@ -2,7 +2,6 @@
 
 <?php
 $answer = 42;
-echo "The answer to the utltimate question of lofe $answer" ;
 ?>
 
 <html>
@@ -12,7 +11,10 @@ echo "The answer to the utltimate question of lofe $answer" ;
 </head>
 
 <body>
-    <h1>Hello from Dr.Chunks!</h1>
+    <h1>Hello from Gilbert</h1>
+    <?php
+    print  "The answer to the utltimate question is $answer";
+    ?>
 </body>
 
 </html>
