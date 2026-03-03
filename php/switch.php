d423cfe68acb0b724577aa9ea18ae86fbdfd18db
Gilbert <gimanishimwe123@gmail.com>
2026-03-03 21:08:39 +0200

    do while + while + switch

---
+++ php/switch.php
@@ -0,0 +1,33 @@
+<?php
+$day = "Wednesday";
+
+switch($day){
+    case "Monday":
+        echo "Today is Monday";
+        break;
+    case "TUesday":
+        echo "Today is Tuesday";
+        break;
+    case "Wednesday":
+        echo "Today is Wwednesday";
+        break;
+    case "Thursday":
+        echo "Today is Thursday";
+        break;
+    case "Friday":
+        echo "Friday";
+        break;
+    case "Saturday":
+        echo "Today is Saturday";
+        break;
+    case "Sunday":
+        echo "Today Sunday";
+        break;
+    default:
+        echo "Invalid day";
+
+}
+
+
+
+?>
