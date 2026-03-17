6a062d456b9d703d4d5b3431b6f33af1b58abd8e
Gilbert <gimanishimwe123@gmail.com>
2026-03-17 19:01:31 +0200

    python

---
+++ python/class.py
@@ -0,0 +1,8 @@
+class point():
+    def __init__(self, input1, input2):
+        self.x =input1
+        self.x =input2
+p = point(2,6)
+l = point(3,4)
+print(p.x)
+print(l.x)
--- python/input.py
+++ python/input.py
@@ -1,6 +1,31 @@
+"""
 rll_nmbr = input("Enter your roll number: ")
 clss = input("Entrt your class: ")
 academic = input("Enter your academic: ")
 
 print("Yello!: ", (rll_nmbr))
 print("Your class is:", (clss), "and your academic is: ", (academic))
+
+
+try:
+    age = int(input("Enter your age: "))
+    
+    if age >= 18:
+        print("you are adult")
+    else:
+        print("You are junior")
+        
+except (ValueError, TypeError):
+    print("Invalid")
+    
+"""
+try:
+    yr = int(input("Enter your year of birth: "))
+    
+    if yr >= 2000:
+        print("you allwoed ")
+    else:
+        print("You are not allowed")
+        
+except (ValueError, TypeError):
+    print("Invalid")
