92f5c4c3643003e25162b61167d7faef57040623
Gilbert <gimanishimwe123@gmail.com>
2026-03-17 18:33:22 +0200

    python

--- python/hello.py
+++ python/hello.py
@@ -52,7 +52,7 @@ print(celsius2)
 
 temp3 = 50
 celsius3 = (temp3 - 32) * 5 / 9
-print(celsius3)  """
+print(celsius3)  
 
 x = 300
 
@@ -64,3 +64,5 @@ myfunc()
 myfunc()
 
 print(x)
+"""
+
---
+++ python/input.py
@@ -0,0 +1,6 @@
+rll_nmbr = input("Enter your roll number: ")
+clss = input("Entrt your class: ")
+academic = input("Enter your academic: ")
+
+print("Yello!: ", (rll_nmbr))
+print("Your class is:", (clss), "and your academic is: ", (academic))
